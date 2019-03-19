fa1064cbe507f8ac421d9a62fdeba0e10a7f65fc
naxus1 <naxus1@gmail.com>
2019-03-19 20:53:40 +0000

    Create function string

--- funtions_pr.c
+++ funtions_pr.c
@@ -1,12 +1,28 @@
 #include "holberton.h"
-
 /**
  * print_char - Print a char
  * @list: argument to print
- *
  */
 int print_char(va_list list)
 {
 	putchar(va_arg(list, int));
 	return (1);
+}
+
+/**
+ * print_string - Pring to string
+ * @list: argument to print
+ */
+int print_string(va_list list)
+{
+	char *p = va_arg(list, char *);
+	int i_st;
+
+	while (p[i_st])
+	{
+		putchar(p[i_st]);
+		i_st++;
+	}
+	return(i_st);
+
 }
