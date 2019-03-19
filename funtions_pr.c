d7e07779135fdadea75fb05aa9626ff737793c14
naxus1 <naxus1@gmail.com>
2019-03-19 20:49:55 +0000

    Create function char

--- funtions_pr.c
+++ funtions_pr.c
@@ -1,3 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_char - Print a char
+ * @list: argument to print
+ *
+ */
 int print_char(va_list list)
-	{ putchar(va_arg(list, int));
+{
+	putchar(va_arg(list, int));
 	return (1);
+}
