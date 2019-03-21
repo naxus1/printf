42823f0ccd1336f20c303d0c51ab8b9d0febee23
naxus1 <naxus1@gmail.com>
2019-03-21 03:49:45 +0000

    modified funtion string

--- point1.c
+++ point1.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 /**
  * _printf - produces output according to a format
-* @format: is a character string
-* Return: number of letter found
-*/
+ * @format: is a character string
+ * Return: number of letter found
+ */
 int _printf(const char *format, ...)
 {
-	int it_for, it_str, cont = 0, change = 0;
+	int it_for, it_str, cont;
 	va_list list;
 	print_f funct[] = {{"c", print_char}, {"s", print_string},
 			   {"i", print_int}, {"d", print_int}};
+
 	va_start(list, format);
+	cont = 0;
+
 	if (format == NULL)
 		return (-1);
-	for (it_for = 0; format[it_for] != '\0'; it_for++)
+
+	for (it_for = 0; format[it_for] != '\0' && format != NULL; it_for++)
 	{
 		if (format[it_for] == '%' && format[it_for + 1] == '%')
 		{
@@ -25,25 +29,18 @@ int _printf(const char *format, ...)
 		else if (format[it_for] == '%')
 		{
 			it_for++;
-			change = 0;
-			for (it_str = 0; it_str < 5; it_str++)
+			for (it_str = 0; it_str < 4; it_str++)
 			{
 				if (format[it_for] == *funct[it_str].flag)
-				{
 					cont += funct[it_str].p(list);
-					change = 1;
-				}
 			}
-			if (change == 0)
-			{
-				_putchar('%');
-				_putchar(format[it_for]);
-			} }
+		}
 		else
 		{
 			_putchar(format[it_for]);
 			cont++;
-		} }
+		}
+	}
 	va_end(list);
 	return (cont);
 }
