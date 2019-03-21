e16fe853b1d4dfb8468ea6336e63af6dfdb693ad
naxus1 <naxus1@gmail.com>
2019-03-21 03:46:47 +0000

    modified funtion string

--- funtions_pr.c
+++ funtions_pr.c
@@ -15,17 +15,18 @@ int print_char(va_list list)
  * print_string - Pring to string
  * @list: argument to print
  * Return: 0 if false, something else otherwise.
-*/
+ */
 int print_string(va_list list)
 {
 	char *p = va_arg(list, char *);
 	int i_st;
 
-		for (i_st = 0; p[i_st] != '\0'; i_st++)
-		{
-			_putchar(p[i_st]);
-		}
-		return (i_st);
+	for (i_st = 0; p[i_st] != '\0', i_st++)
+	{
+		_putchar(p[i_st]);
+	}
+	return (i_st);
+
 }
 
 /**
@@ -36,17 +37,13 @@ int print_string(va_list list)
 int print_int(va_list list)
 {
 	int p = va_arg(list, int), rev_num = 0;
-	int crash = 0, result = 0, cont = 0, last_digit = 0;
+	int crash = 0, result = 0;
 
 	if (p < 0)
 	{
 		_putchar('-');
 		p *= -1;
 	}
-
-	last_digit = p % 10;
-	p = p / 10;
-
 	while (p > 0)
 	{
 		rev_num = rev_num * 10 + p % 10;
@@ -60,16 +57,11 @@ int print_int(va_list list)
 			result = rev_num % 10;
 			_putchar(result + '0');
 			rev_num = rev_num / 10;
-			cont++;
 		}
 		else
 		{
 			_putchar(rev_num + '0');
 			crash = 1;
-			cont++;
 		}
 	}
-	_putchar(last_digit + '0');
-	cont++;
-	return (cont);
 }
